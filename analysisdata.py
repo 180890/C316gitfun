4a7cab532237201d1859fa20911dde40a49c071b
ananya-dak <ananyadakamuri@gmail.com>
2022-10-18 22:36:48 +0530

    Update analysisdata.py

--- analysisdata.py
+++ analysisdata.py
@@ -1,9 +1,9 @@
 import pandas as pd
 df = pd.read_csv(r'C:\Users\anany\OneDrive\Documents\GitHub\C316gitfun\BigmacPrice.csv')
 print(df)
-mean = df['local_price'].mean()
-print("mean : ",mean)
+mean = df['dollar_price'].mean()
+print("mean of dollar_price: ",mean)
 std = df['local_price'].std()
-print("standard deviation : ",std)
+print("standard deviation of dollar_price: ",std)
 median = df['local_price'].median()
-print("median : ",median)
+print("median of dollar_price: ",median)
